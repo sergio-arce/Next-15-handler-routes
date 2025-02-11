23fafec482f111f654a55ca8d20986cec18ff633
Sergio Arce <arcesech30@gmail.com>
2025-02-11 11:52:35 +0100

    add handle POST in comments data

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -3,3 +3,22 @@ import { comments } from "./data"
 export async function GET() {
   return Response.json(comments)
 }
+
+export async function POST(req: Request) {
+
+  const comment = await req.json()
+
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text
+  }
+  comments.push(newComment)
+
+  console.log(comments)
+
+  return new Response(JSON.stringify(newComment), {
+    headers: { "Conten-Type": "application/json" },
+    status: 201
+  })
+
+}
