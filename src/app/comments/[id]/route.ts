f1dfb40a78d0cefe8b8c7ed5a01e9064067b7313
Sergio Arce <arcesech30@gmail.com>
2025-02-11 17:49:26 +0100

    add handle DELETE in comments

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -18,5 +18,19 @@ export async function PATCH(req: Request, { params }: { params: Promise<{ id: st
   comments[index].text = text
 
   return Response.json(comments[index])
+}
 
+export async function DELETE(req: Request, { params }: { params: Promise<{ id: string }>}) {
+  const { id } = await params
+  
+  const index = comments.findIndex((comment) => comment.id === parseInt(id))
+
+  if (index === -1) {
+    return new Response(JSON.stringify({ error: "Comment no found"}), { status: 404 })
+  }
+  const deleteComment = comments[index]
+  comments.splice(index, 1)
+
+  // return new Response(JSON.stringify({ message: `Comment with id: ${ id } deleted successfully`}), { status: 200 })
+  return Response.json(deleteComment)
 }
