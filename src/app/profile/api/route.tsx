fc1caa904d11c2a4a4547f2dd6a59da07fa3a602
Sergio Arce <arcesech30@gmail.com>
2025-02-12 11:38:51 +0100

    add headers in handle routes in profile/api

--- src/app/profile/api/route.tsx
+++ src/app/profile/api/route.tsx
@@ -1,3 +1,25 @@
-export async function GET() {
-  return new Response('Profile route')
+// export async function GET() {
+//   return new Response('Profile route')
+// }
+
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+
+export async function GET(req: NextRequest) {
+  /**
+   * Opcion 1
+   */
+  // const requestHeaders = new Headers(req.headers)
+  // console.log(requestHeaders.get('Authorization'))
+
+  /**
+   * Option Next use headers
+   */
+  const headerList = await headers()
+  console.log(headerList.get("Authorization"))
+
+  return new Response('<h1 style="color: red">Profile route</h1>', {
+    headers: { "Content-Type": "text/html"}
+  })
 }
