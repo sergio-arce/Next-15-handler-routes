03ea499bd6e2fe5059b68bcfbb0b8be731cfe275
Sergio Arce <arcesech30@gmail.com>
2025-02-12 11:58:02 +0100

    add cookies in handle routes

--- src/app/profile/api/route.tsx
+++ src/app/profile/api/route.tsx
@@ -2,13 +2,13 @@
 //   return new Response('Profile route')
 // }
 
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 
 export async function GET(req: NextRequest) {
   /**
-   * Opcion 1
+   * Option 1
    */
   // const requestHeaders = new Headers(req.headers)
   // console.log(requestHeaders.get('Authorization'))
@@ -19,7 +19,25 @@ export async function GET(req: NextRequest) {
   const headerList = await headers()
   console.log(headerList.get("Authorization"))
 
+  const theme = req.cookies.get('theme')
+  console.log(theme)
+
+  /**
+   * Option 2 set and get cookiens
+   * there are has and delete methods
+   */
+
+  const cookiesStore = await cookies()
+  cookiesStore.set('resultPerPage', "30")
+  console.log(cookiesStore.get("resultPerPage"))
+
   return new Response('<h1 style="color: red">Profile route</h1>', {
-    headers: { "Content-Type": "text/html"}
+    headers: { 
+      "Content-Type": "text/html",
+      /**
+       * Option 1 set cookies
+       */
+      "Set-Cookie": "theme=dark"
+    }
   })
 }
